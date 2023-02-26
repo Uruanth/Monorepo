5e7a1ca2b94c0c254b4698d709aa9696932769f3
Dairon Perilla <donperrydev@gmail.com>
2023-02-26 10:14:49 -0500

    gateway

---
+++ Apps/gateway/src/main/java/com/pragma/GatewayApp.java
@@ -0,0 +1,34 @@
+package com.pragma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
+}
