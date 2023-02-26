dd31640bd7c0bf4f89a9520b43f927c02f9f41ea
Dairon Perilla <donperrydev@gmail.com>
2023-02-26 11:49:39 -0500

    restaurant Domain

--- Domain/src/main/java/com/pragma/plazoleta/Restaurant.java
+++ Domain/src/main/java/com/pragma/plazoleta/Restaurant.java
@@ -6,4 +6,13 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class Restaurant {
+
+    private Integer id;
+    private String name;
+    private String addres;
+    private String id_owner;
+    private String phone;
+    private String urlLogo;
+    private Integer nit;
+
 }
